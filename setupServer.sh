3b92b0e1184f4f1b23867c906fff77c012658bb0
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2023-03-04 15:53:49 +0000

    Add V1 of setup script

---
+++ setupServer.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+echo "Setting up server ping API"
+echo "Installing gh and git"
+sudo apt install gh git
+gh auth login
+echo Creating user
+useradd --disabled-password --gecos "" pingChecker
+echo "move to user home"
+cd /home/pingChecker
+echo "clone repo"
+git clone https://github.com/Otter-Bots/status.git
+cd status
+echo "Clear up clone"
+rm -rf ./.git
+rm -rf ./frontend/
+rm -rf ./LICENSE README.md
+mv ./serverPingAPI/ /home/pingChecker
+echo "Install dependencies"
+yarn install
+echo "Setup .env"
+echo "PORT=7681" >> .env
